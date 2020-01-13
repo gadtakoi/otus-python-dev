2275cf7cf56311a11d19b55a302e7fff3897007a
stas <ivanchenko@gambit.ru>
2020-01-13 18:34:47 +0300

    ДЗ-4: Web server. Fix 1

--- dz4/README.md
+++ dz4/README.md
@@ -16,49 +16,62 @@ Core i5-3470 CPU @ 3.20GHz, 4 cores, 16RAM, SSD
 
 ### Load test results:
 ```shell script
-ab -n 50000 -c 100 -r http://localhost:8080/
+ab -n 50000 -c 100 -r http://127.0.0.1:80/httptest/dir2/
 ```
 
 ```
 Server Software:        OTUServer
 Server Hostname:        localhost
-Server Port:            8080
+Server Port:            80
 
-Document Path:          /
-Document Length:        0 bytes
+Document Path:          /httptest/dir2/
+Document Length:        34 bytes
 
 Concurrency Level:      100
-Time taken for tests:   4.628 seconds
+Time taken for tests:   7.477 seconds
 Complete requests:      50000
 Failed requests:        0
-Non-2xx responses:      50000
-Total transferred:      5050000 bytes
-HTML transferred:       0 bytes
-Requests per second:    10803.70 [#/sec] (mean)
-Time per request:       9.256 [ms] (mean)
-Time per request:       0.093 [ms] (mean, across all concurrent requests)
-Transfer rate:          1065.60 [Kbytes/sec] received
+Total transferred:      8650000 bytes
+HTML transferred:       1700000 bytes
+Requests per second:    6687.18 [#/sec] (mean)
+Time per request:       14.954 [ms] (mean)
+Time per request:       0.150 [ms] (mean, across all concurrent requests)
+Transfer rate:          1129.77 [Kbytes/sec] received
 
 Connection Times (ms)
               min  mean[+/-sd] median   max
-Connect:        0    2   1.4      1       9
-Processing:     0    8   4.1      7      31
-Waiting:        0    6   3.7      5      29
-Total:          0    9   4.0      9      33
+Connect:        0    1   0.9      1       7
+Processing:     1   14   6.4     13      57
+Waiting:        0   12   5.8     11      56
+Total:          1   15   6.5     14      59
 
 Percentage of the requests served within a certain time (ms)
-  50%      9
-  66%     10
-  75%     11
-  80%     12
-  90%     14
-  95%     17
-  98%     21
-  99%     24
- 100%     33 (longest request)
+  50%     14
+  66%     16
+  75%     18
+  80%     19
+  90%     23
+  95%     26
+  98%     31
+  99%     33
+ 100%     59 (longest request)
 ```
 
 ### Web server test suite:
 ```git
 https://github.com/s-stupnikov/http-test-suite
+```
+
+### Check ../
+
+```bash
+telnet 127.0.0.1 80
+```
+```
+GET /httptest/dir2/../../../../zzz.html HTTP/1.1
+```
+
+### Check length in HEAD 
+```bash
+curl -I http://127.0.0.1/httptest/dir2/
 ```
--- dz4/httpd.py
+++ dz4/httpd.py
@@ -42,164 +42,164 @@ class Codes:
         return ' '.join([HTTP_VERSION_STRING, str(code), self.description[code]])
 
 
-class OTUServer(object):
-    def __init__(self, address, port, rootdir, workers_count):
-        self.address = address
-        self.port = port
-        self.root_path = os.path.abspath(rootdir)
-        self.workers_count = workers_count
-        self._workers = list()
+_workers = list()
 
-    def _serve(self, sock):
-        selector = selectors.EpollSelector()
-        loop = asyncio.SelectorEventLoop(selector)
-        asyncio.set_event_loop(loop)
-        coro = asyncio.start_server(self._handle_connection, sock=sock)
-        server = loop.run_until_complete(coro)
-        loop.add_signal_handler(signal.SIGTERM, loop.stop)
-        loop.add_signal_handler(signal.SIGINT, loop.stop)
-        logging.info('Starting server worker at http://{}:{}'.format(self.address, self.port))
+
+def _serve(sock):
+    selector = selectors.EpollSelector()
+    loop = asyncio.SelectorEventLoop(selector)
+    asyncio.set_event_loop(loop)
+    coro = asyncio.start_server(_handle_connection, sock=sock)
+    server = loop.run_until_complete(coro)
+    loop.add_signal_handler(signal.SIGTERM, loop.stop)
+    loop.add_signal_handler(signal.SIGINT, loop.stop)
+    logging.info('Starting server worker at http://{}:{}'.format(address, port))
+    try:
+        loop.run_forever()
+    finally:
+        logging.info('Closing server worker...')
+        server.close()
+        coro.close()
+        loop.run_until_complete(coro)
+
+
+def _terminate(unused1, unused2):
+    logging.info('Termination request received, shutting down workers.')
+    for worker in _workers:
+        worker.terminate()
+
+
+def _socket():
+    _sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    _sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    _sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    _sock.bind((address, port))
+    return _sock
+
+
+def start():
+    sock = _socket()
+    signal.signal(signal.SIGINT, _terminate)
+    signal.signal(signal.SIGTERM, _terminate)
+    for _ in range(workers_count):
+        worker = mp.Process(target=_serve, kwargs=dict(sock=sock))
+        worker.daemon = True
+        worker.start()
+        _workers.append(worker)
+
+    for worker in _workers:
+        worker.join()
+
+
+async def _handle_connection(reader, writer):
+    address = writer.get_extra_info('peername')
+    logging.info('Accepted connection from %s.', address)
+    raw_request = None
+    content = None
+    headers = HEADERS_EMPTY_CONTENT
+    while True:
         try:
-            loop.run_forever()
-        finally:
-            logging.info('Closing server worker...')
-            server.close()
-            coro.close()
-            loop.run_until_complete(coro)
-
-    def _terminate(self, unused1, unused2):
-        logging.info('Termination request received, shutting down workers.')
-        for worker in self._workers:
-            worker.terminate()
-
-    def _socket(self):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self._sock.bind((self.address, self.port))
-
-    def start(self):
-        self._socket()
-        signal.signal(signal.SIGINT, self._terminate)
-        signal.signal(signal.SIGTERM, self._terminate)
-        for _ in range(self.workers_count):
-            worker = mp.Process(target=self._serve, kwargs=dict(sock=self._sock))
-            worker.daemon = True
-            worker.start()
-            self._workers.append(worker)
-
-        for worker in self._workers:
-            worker.join()
-
-    async def _handle_connection(self, reader, writer):
-        address = writer.get_extra_info('peername')
-        logging.info('Accepted connection from %s.', address)
-        raw_request = None
-        content = None
-        headers = HEADERS_EMPTY_CONTENT
-        while True:
-            try:
-                raw_request = await reader.readuntil(HEADER_END.encode())
-                if raw_request:
-                    break
-            except asyncio.IncompleteReadError:
+            raw_request = await reader.readuntil(HEADER_END.encode())
+            if raw_request:
                 break
-        try:
-            method, resource, response_headers = self._parse_request(raw_request)
-            path = self._parse_path(resource)
-            if method in ['GET', 'HEAD']:
-                code, headers = self._prepare_response(path, response_headers)
-                if method == 'GET' and code == Codes.OK:
-                    with open(os.path.join(self.root_path, path), 'rb') as fd:
-                        try:
-                            content = await self._read_file(fd)
-                        except IOError:
-                            logging.error('Error on reading requested file %s contents.', path)
-                            raise
-            else:
-                code = Codes.NOT_ALLOWED
-        except Exception as e:
-            logging.exception(e)
-            code = Codes.SERVER_ERROR
-        writer.write(self._create_header_lines(code, headers))
-        if content:
-            writer.write(content)
-        await writer.drain()
-        writer.close()
-
-    def _parse_request(self, raw_request):
-        if not raw_request:
-            raise Exception('Error on reading request data, no data was read')
-        decoded_request = raw_request.decode('utf-8')
-        splitted_request = decoded_request.split(LINE_END)
-        request_line, header_lines = splitted_request[0], splitted_request[1:]
-        request_args = request_line.split()
-        if len(request_args) < 2:
-            raise Exception('Wrong request line: %s' % request_line)
-        method, resource = request_args[:2]
-        headers = self._parse_headers(header_lines)
-        close_connection = headers.get('Connection', 'close') == 'close'
-        connection = 'close' if close_connection else 'keep-alive'
-        response_headers = [
-            'Date: {}'.format(strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())),
-            'Server: OTUServer',
-            'Connection: {}'.format(connection)
-        ]
-        return method, resource, response_headers
-
-    def _parse_headers(self, header_lines):
-        out = dict()
-        for line in header_lines:
-            line = line.strip()
-            if line:
-                header, value = line.split(': ')
-                out[header] = value
-        return out
-
-    def _parse_path(self, resource):
-        unquoted_resource = unquote(resource)
-        path = unquoted_resource
-        if '?' in unquoted_resource:
-            path, unused_query_string = unquoted_resource.split('?')
-        if path.endswith('/'):
-            path = path + 'index.html'
-        if path.startswith('/'):
-            path = path[1:]
-        return path
-
-    def _prepare_response(self, document_path, headers):
-        full_path = os.path.join(self.root_path, document_path)
-        file_exists = os.path.exists(full_path)
-        escaping = file_exists and '/..' in full_path
-        if escaping:
-            return Codes.FORBIDDEN, headers
-        if not file_exists:
-            return Codes.NOT_FOUND, headers
-
-        path = os.path.join(self.root_path, document_path)
-        content_length = os.path.getsize(path)
-        _, ext = os.path.splitext(path)
-        content_type = mimetypes.types_map[ext.lower()]
-        headers.extend([
-            'Content-Length: {}'.format(content_length),
-            'Content-Type: {}'.format(content_type)
-        ])
-        return Codes.OK, headers
-
-    def _create_header_lines(self, http_code, headers):
-        lines = [Codes().to_response_line(http_code)] + headers
-        response_string = LINE_END.join(lines) + HEADER_END
-        return response_string.encode()
-
-    def _read_file(self, file):
-        loop = asyncio.get_event_loop()
-        return loop.run_in_executor(None, file.read)
+        except asyncio.IncompleteReadError:
+            break
+    try:
+        method, resource, response_headers = _parse_request(raw_request)
+        path = _parse_path(resource)
+        if method in ['GET', 'HEAD']:
+            code, headers = _prepare_response(path, response_headers)
+            if method == 'GET' and code == Codes.OK:
+                with open(os.path.join(root_path, path), 'rb') as fd:
+                    try:
+                        content = await _read_file(fd)
+                    except IOError:
+                        logging.error('Error on reading requested file %s contents.', path)
+                        raise
+        else:
+            code = Codes.NOT_ALLOWED
+    except Exception as e:
+        logging.exception(e)
+        code = Codes.SERVER_ERROR
+    writer.write(_create_header_lines(code, headers))
+    if content:
+        writer.write(content)
+    await writer.drain()
+    writer.close()
 
 
-def main(opts):
-    workers = min(mp.cpu_count(), opts.workers)
-    server = OTUServer('127.0.0.1', opts.port, opts.rootdir, workers)
-    server.start()
+def _parse_request(raw_request):
+    if not raw_request:
+        raise Exception('Error on reading request data, no data was read')
+    decoded_request = raw_request.decode('utf-8')
+    splitted_request = decoded_request.split(LINE_END)
+    request_line, header_lines = splitted_request[0], splitted_request[1:]
+    request_args = request_line.split()
+    if len(request_args) < 2:
+        raise Exception('Wrong request line: %s' % request_line)
+    method, resource = request_args[:2]
+    headers = _parse_headers(header_lines)
+    close_connection = headers.get('Connection', 'close') == 'close'
+    connection = 'close' if close_connection else 'keep-alive'
+    response_headers = [
+        'Date: {}'.format(strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())),
+        'Server: OTUServer',
+        'Connection: {}'.format(connection)
+    ]
+    return method, resource, response_headers
+
+
+def _parse_headers(header_lines):
+    out = dict()
+    for line in header_lines:
+        line = line.strip()
+        if line:
+            header, value = line.split(': ')
+            out[header] = value
+    return out
+
+
+def _parse_path(resource):
+    unquoted_resource = unquote(resource)
+    path = unquoted_resource
+    if '?' in unquoted_resource:
+        path, unused_query_string = unquoted_resource.split('?')
+    if path.endswith('/'):
+        path = path + 'index.html'
+    if path.startswith('/'):
+        path = path[1:]
+    return path
+
+
+def _prepare_response(document_path, headers):
+    normpath = os.path.normpath(document_path)
+    full_path = os.path.join(root_path, normpath)
+    if '/..' in full_path:
+        return Codes.FORBIDDEN, headers
+    file_exists = os.path.exists(full_path)
+    if not file_exists:
+        return Codes.NOT_FOUND, headers
+
+    path = os.path.join(root_path, document_path)
+    content_length = os.path.getsize(path)
+    _, ext = os.path.splitext(path)
+    content_type = mimetypes.types_map[ext.lower()]
+    headers.extend([
+        'Content-Length: {}'.format(content_length),
+        'Content-Type: {}'.format(content_type)
+    ])
+    return Codes.OK, headers
+
+
+def _create_header_lines(http_code, headers):
+    lines = [Codes().to_response_line(http_code)] + headers
+    response_string = LINE_END.join(lines) + HEADER_END
+    return response_string.encode()
+
+
+def _read_file(file):
+    loop = asyncio.get_event_loop()
+    return loop.run_in_executor(None, file.read)
 
 
 if __name__ == "__main__":
@@ -214,4 +214,10 @@ if __name__ == "__main__":
                         level=logging.INFO,
                         format='[%(asctime)s] %(levelname).1s %(message)s',
                         datefmt='%Y.%m.%d %H:%M:%S')
-    main(opts)
+
+    workers = min(mp.cpu_count(), opts.workers)
+    address = '127.0.0.1'
+    port = opts.port
+    root_path = os.path.abspath(opts.rootdir)
+    workers_count = workers
+    start()
