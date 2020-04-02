6cfa5bb9bc761a8ccdea366ea3624577ce65d95a
stas <gadtakoi@gmail.com>
2020-04-02 12:35:18 +0300

    ДЗ-9: MemcLoad + timeouts, chunks and do TODO

--- dz9/memc_load.py
+++ dz9/memc_load.py
@@ -10,13 +10,18 @@ from queue import Queue, Empty
 from threading import Thread
 from time import sleep
 
-
 import appsinstalled_pb2
 import memcache
 
 NORMAL_ERR_RATE = 0.01
 AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
 FINISH = 'finish'
+SOCKET_TIMEOUT = 2
+DEAD_RETRY = 20
+RETRIES = 3
+RETRIES_SLEEP = 1
+GET_TIMEOUT = 0.1
+CHUNK_SIZE = 10
 
 
 def dot_rename(path):
@@ -25,24 +30,42 @@ def dot_rename(path):
     os.rename(path, os.path.join(head, "." + fn))
 
 
-def insert_appsinstalled(memc, appsinstalled, dry_run=False):
+def insert_appsinstalled(memc, chunks: dict, dry_run=False):
+    processed = errors = 0
+    try:
+        if dry_run:
+            for key, value in chunks.items():
+                logging.debug('%s - %s -> %s' % (memc, key, value))
+                processed += 1
+        else:
+            processed, errors = memcache_set(memc, chunks)
+    except Exception as e:
+        logging.exception("Cannot write to memc %s: %s" % (memc, e))
+    return processed, errors
+
+
+def memcache_set(memc, chunks):
+    notset_keys = memc.set_multi(chunks)
+    retries = 0
+    while notset_keys and retries < RETRIES:
+        notset_keys = memc.set_multi({
+            key: chunks[key]
+            for key in notset_keys
+        })
+        retries += 1
+        sleep(RETRIES_SLEEP)
+    errors = len(notset_keys)
+    return len(chunks.keys()) - errors, errors
+
+
+def prepare_appsinstalled(appsinstalled):
     ua = appsinstalled_pb2.UserApps()
     ua.lat = appsinstalled.lat
     ua.lon = appsinstalled.lon
-    key = "%s:%s" % (appsinstalled.dev_type, appsinstalled.dev_id)
+    key = '%s:%s' % (appsinstalled.dev_type, appsinstalled.dev_id)
     ua.apps.extend(appsinstalled.apps)
     packed = ua.SerializeToString()
-    # @TODO persistent connection
-    # @TODO retry and timeouts!
-    try:
-        if dry_run:
-            logging.debug("%s - %s -> %s" % (memc, key, str(ua).replace("\n", " ")))
-        else:
-            memc.set(key, packed)
-    except Exception as e:
-        logging.exception("Cannot write to memc %s: %s" % (memc, e))
-        return False
-    return True
+    return (key, packed)
 
 
 def parse_appsinstalled(line):
@@ -77,20 +100,28 @@ class Worker(Thread):
         logging.info('[Worker %s] Start thread: %s' % (os.getpid(), self.name))
         processed = 0
         errors = 0
+        chunk = dict()
         while True:
             try:
-                line = self.job_pool.get(timeout=0.01)
+                try:
+                    line = self.job_pool.get(timeout=GET_TIMEOUT)
+                except Empty:
+                    continue
+
+                chunk[line[0]] = line[1]
+
                 if line == FINISH:
                     self.results_queue.put((processed, errors))
                     self.job_pool.task_done()
                     logging.info('[Worker %s] Stop thread: %s' % (os.getpid(), self.name))
                     break
                 else:
-                    if not insert_appsinstalled(self.memc, line):
-                        errors += 1
-                    else:
-                        processed += 1
-                    self.job_pool.task_done()
+                    if len(chunk) == CHUNK_SIZE:
+                        proc_items, err_items = insert_appsinstalled(self.memc, chunk)
+                        processed += proc_items
+                        errors += err_items
+                        chunk = dict()
+                        self.job_pool.task_done()
             except Empty:
                 continue
 
@@ -100,7 +131,7 @@ def file_handler(fn, options, device_memc):
     treads_pool = list()
     results_queue = Queue()
     for dev_type, address in device_memc.items():
-        memc = memcache.Client(servers=[address])
+        memc = memcache.Client(servers=[address], socket_timeout=SOCKET_TIMEOUT, dead_retry=DEAD_RETRY)
         jobs_pool[dev_type] = Queue()
         thread = Worker(jobs_pool[dev_type], memc, results_queue, options.dry)
         treads_pool.append(thread)
@@ -115,7 +146,7 @@ def file_handler(fn, options, device_memc):
         if not line:
             continue
         appsinstalled = parse_appsinstalled(line)
-        jobs_pool[appsinstalled.dev_type].put(appsinstalled)
+        jobs_pool[appsinstalled.dev_type].put(prepare_appsinstalled(appsinstalled))
 
         count = count + 1
         if count % 300000 == 0:
@@ -191,7 +222,7 @@ if __name__ == '__main__':
     op.add_option("--gaid", action="store", default="127.0.0.1:33014")
     op.add_option("--adid", action="store", default="127.0.0.1:33015")
     op.add_option("--dvid", action="store", default="127.0.0.1:33016")
-    op.add_option('-w', '--workers', action='store', type=int, default=cpu_count()+1)
+    op.add_option('-w', '--workers', action='store', type=int, default=cpu_count() + 1)
 
     (opts, args) = op.parse_args()
     logging.basicConfig(filename=opts.log, level=logging.INFO if not opts.dry else logging.DEBUG,
